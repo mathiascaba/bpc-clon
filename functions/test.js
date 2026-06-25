f9962fd83e34ae66ad0e34dc2d2f099de50d9b7a
Mathias <mathias@example.com>
2026-06-25 01:47:23 -0500

    Check blob env var value

--- functions/test.js
+++ functions/test.js
@@ -2,6 +2,10 @@ exports.handler = async (event, context) => {
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(Object.keys(process.env).sort())
+    body: JSON.stringify({
+      NETLIFY_BLOBS_CONTEXT: process.env.NETLIFY_BLOBS_CONTEXT,
+      NETLIFY_FUNCTIONS_TOKEN: process.env.NETLIFY_FUNCTIONS_TOKEN ? 'present' : 'missing',
+      NETLIFY_PURGE_API_TOKEN: process.env.NETLIFY_PURGE_API_TOKEN ? 'present' : 'missing'
+    })
   }
 }
